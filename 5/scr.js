863f953cd168ce41ede6b616b764402008591753
Deinare <yala_anosova@hotmail.com>
2024-10-07 12:08:22 +0300

    Update scr.js

--- 5/scr.js
+++ 5/scr.js
@@ -1,6 +1,9 @@
-function resultat() {
-  let f1 = document.getElementsByName("product");
-  let f2 = document.getElementsByName("kol");
-  let res = document.getElementsByName("result");
- res.innerHTML =f1[0].value*f2[0].value;
+function click1() {
+  let f1 = document.getElementsByName("field1");
+  let f2 = document.getElementsByName("field2");
+  let r = document.getElementById("result");
+  r.innerHTML = f1[0].value + f2[0].value;
+  let s = document.getElementsByName("select1");
+  console.log(s[0].value);
+  return false;
 }
