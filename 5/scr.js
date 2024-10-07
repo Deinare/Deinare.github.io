fe87d942d6547e769cd2b2f1fd27f5916ad90cbb
Deinare <yala_anosova@hotmail.com>
2024-10-07 12:00:48 +0300

    Update scr.js

--- 5/scr.js
+++ 5/scr.js
@@ -4,8 +4,3 @@ function resultat(event) {
   let f2 = document.getElementByName("result");
   return f1.value*f2.value;
 }
-window.addEventListener('DOMContentLoaded', function (event) {
-  console.log("DOM fully loaded and parsed");
-  let b = document.getElementById("my-button");
-  b.addEventListener(b, resultat);
-});
