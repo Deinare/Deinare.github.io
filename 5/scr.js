730fbc17fb0fb599285663b76b6e817ae637fd38
Deinare <yala_anosova@hotmail.com>
2024-10-07 12:04:06 +0300

    Update scr.js

--- 5/scr.js
+++ 5/scr.js
@@ -1,6 +1,6 @@
 function resultat(event) {
   let f1 = document.getElementsByName("product");
   let f2 = document.getElementsByName("kol");
-  let f2 = document.getElementsByName("result");
-  return f1[0].value*f2[0].value;
+  let res = document.getElementsByName("result");
+ res.innerHTML =f1[0].value*f2[0].value;
 }
