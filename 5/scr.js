01b655eb22dbbe5b6d7914067a571b495b59aede
Deinare <yala_anosova@hotmail.com>
2024-10-07 12:10:54 +0300

    Update scr.js

--- 5/scr.js
+++ 5/scr.js
@@ -1,9 +1,7 @@
 function click1() {
-  let f1 = document.getElementsByName("field1");
-  let f2 = document.getElementsByName("field2");
+  let f1 = document.getElementsByName("product");
+  let f2 = document.getElementsByName("kol");
   let r = document.getElementById("result");
   r.innerHTML = f1[0].value + f2[0].value;
-  let s = document.getElementsByName("select1");
-  console.log(s[0].value);
-  return false;
+  
 }
