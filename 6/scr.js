6e09fd90309c63fd1bec908c64954f7bbeadfc2a
Deinare <yala_anosova@hotmail.com>
2024-10-16 13:06:01 +0300

    Update scr.js

--- 6/scr.js
+++ 6/scr.js
@@ -5,43 +5,44 @@ window.addEventListener("DOMContentLoaded", function () {
         { "1": 1, "2": 1, "3": 2, "4": 2, "5": 3, "6": 4 };
     const colormn =
         { "col1": 1.4, "col2": 0.9, "col3": 1, "col4": 1.5, "col5": 1.1 };
-    ///переменные
+
     let k = document.getElementById("kol");
     let product = document.getElementById("vybor");
-    let result = document.getElementById("resultat");
-    let colorList = document.getElementById("colo");
+    let result = document.getElementById("result"); colo
+    let colorlist = document.getElementById("colo");
     let ysluga = document.getElementsByName("ys");
-    let dopolnit = document.getElementsByName("dopol");
-    ///блоки
+    let port= document.getElementsByName("dopol");
+    //блоки
     let c = document.getElementById("colors");
-    let d = document.getElementById("dop");
-
+    let d= document.getElementById("dop");
+    //счет
     function calculate() {
         let kol = k.value;
         if (kol !== "") {
             if (kol.match(/^\d+$/) !== null) {
                 //переменные+услуга
                 let col = 1;
-                let serv = (ysluga[0].checked * (10000)) + (ysluga[1].checked * (12000));
+                let service = (ysluga[0].checked * (1000)) + (ysluga[1].checked * (1200));
                 let karkas = 0;
                 let drevo = 0;
 
                 //цвета
-                if (category[product.value] === 2 || category[product.value] === 3) {
-                    col = colormn[colorList.value];
+                if (category[product.value] === 2 || category[product.value]===3) {
+                    col = colormn[colorlist.value];
                 }
+                
 
                 //дополнительно
                 if (category[product.value] === 1 || category[product.value] === 2) {
-                    if (dopolnit[0].checked === true) {
-                        karkas = 2400;
-                    }
-                    if (dopolnit[1].checked === true) {
-                        drevo = 3000;
-                    }
-                }
+                    if (port[0].checked === true) {
+                        karkas = 1000;
+                    } 
+                    if (port[1].checked === true) {
+                        drevo = 1500;
+                    } 
+                } 
 
-                let res = cena[product.value] * kol * col + drevo + serv + karkas;
+                let res = cena[product.value] * kol * col + drevo + service + karkas;
                 result.innerText = res;
             } else {
                 result.innerText = "Некорректный ввод количества";
@@ -58,7 +59,7 @@ window.addEventListener("DOMContentLoaded", function () {
         });
     });
 
-    dopolnit.forEach(function (elem) {
+    port.forEach(function (elem) {
         elem.addEventListener("change", function () {
             calculate();
         });
@@ -68,25 +69,25 @@ window.addEventListener("DOMContentLoaded", function () {
         calculate();
     });
 
-    colorList.addEventListener("change", function () {
+    colorlist.addEventListener("change", function () {
         calculate();
     });
-    ///на экран
+
     product.addEventListener("change", function () {
-        if (category[product.value] === 2) {
-            calculate();
-            d.style = "display:flex; flex-direction: column";
-            c.style = "display:flex; flex-direction: column";
-        }
         if (category[product.value] === 3) {
             calculate();
             d.style = "display:none";
-            c.style = "display:flex; flex-direction: columne";
+            c.style = "display:flex; flex-direction: column";
         }
         if (category[product.value] === 1) {
             calculate();
             d.style = "display:flex; flex-direction: column";
             c.style = "display:none";
+        }
+        if (category[product.value] === 2) {
+            calculate();
+            d.style = "display:flex; flex-direction: column";
+            c.style = "display:flex; flex-direction: column";
         }
         if (category[product.value] === 4) {
             calculate();
