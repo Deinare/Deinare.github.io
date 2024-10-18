f137aa6dfe7b9e7fafadd7839ec4403c169403b9
Deinare <yala_anosova@hotmail.com>
2024-10-18 17:39:58 +0300

    Update scr.js

--- 6/scr.js
+++ 6/scr.js
@@ -1,25 +1,29 @@
 window.addEventListener("DOMContentLoaded", function () {
+    //соответствие объекта с категорией или стоимостью
     const cena =
         { "1": 1000, "2": 1540, "3": 25000, "4": 12500, "5": 999, "6": 560 };
     const category =
         { "1": 1, "2": 1, "3": 2, "4": 2, "5": 3, "6": 4 };
     const colormn =
         { "col1": 1.4, "col2": 0.9, "col3": 1, "col4": 1.5, "col5": 1.1 };
-
+    // переменные
     let k = document.getElementById("kol");
     let product = document.getElementById("vybor");
     let result = document.getElementById("result"); colo
     let colorlist = document.getElementById("colo");
     let ysluga = document.getElementsByName("ys");
-    let port= document.getElementsByName("dopol");
+    let port = document.getElementsByName("dopol");
     //блоки
     let c = document.getElementById("colors");
-    let d= document.getElementById("dop");
-    let s= document.getElementsByName("service");
+    let d = document.getElementById("dop");
+    let s = document.getElementById("service");
     //счет
     function calculate() {
         let kol = k.value;
-       
+        if (kol === "") {
+            result.innerText = "";
+        }
+        else {
             if (kol.match(/^\d+$/) !== null) {
                 //переменные+услуга
                 let col = 1;
@@ -28,34 +32,33 @@ window.addEventListener("DOMContentLoaded", function () {
                 let drevo = 0;
 
                 //цвета
-                if (category[product.value] === 2 || category[product.value]===3) {
-                    col = colormn[colorlist.value]*kol;
+                if (category[product.value] === 2 || category[product.value] === 3) {
+                    col = colormn[colorlist.value] * kol;
                 }
                 //услуга
-                if (category[product.value] === 1 || category[product.value] === 2)
-                { 
-                    service=(ysluga[0].checked * (1000)) + (ysluga[1].checked * (1200));
+                if (category[product.value] === 1 || category[product.value] === 2) {
+                    service = (ysluga[0].checked * (1000)) + (ysluga[1].checked * (1200));
                 }
                 //дополнительно
                 if (category[product.value] === 1 || category[product.value] === 2) {
                     if (port[0].checked === true) {
-                        karkas = 1000*kol;
-                    } 
+                        karkas = 1000 * kol;
+                    }
                     if (port[1].checked === true) {
-                        drevo = 1500*kol;
-                    } 
-                } 
+                        drevo = 1500 * kol;
+                    }
+                }
 
-                let res = cena[product.value] * col + drevo + service +karkas;
+                let res = cena[product.value] * col + drevo + service + karkas;
                 result.innerText = res;
-            } 
+            }
             else {
                 result.innerText = "Некорректный ввод количества";
             }
-        
-       
-    }
+        }
 
+    }
+    ///вызов
     ysluga.forEach(function (elem) {
         elem.addEventListener("change", function () {
             calculate();
@@ -75,7 +78,7 @@ window.addEventListener("DOMContentLoaded", function () {
     colorlist.addEventListener("change", function () {
         calculate();
     });
-
+    //показ на экран объектов
     product.addEventListener("change", function () {
         if (category[product.value] === 3) {
             calculate();
